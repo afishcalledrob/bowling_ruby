d816f54738cbc22df1e30d9422c63830f189a06f
Robert <fishwickrob@live.com>
2018-08-23 15:38:20 +0000

    total score method for perfect game

--- lib/score.rb
+++ lib/score.rb
@@ -1,4 +1,6 @@
 class Score
+    
+    attr_reader :frame, :frame_index, :rolls
 
   def initialize
     @total_score = 0
@@ -6,11 +8,19 @@ class Score
 
   def total_score(frames)
     @total_frames = frames
-    @total_frames.each_with_index { |frame, index| frame_score(frame, index) }
+    if perfect_game?
+        @total_score += 300
+    else   
+      @total_frames.each_with_index { |frame, index| frame_score(frame, index) }
+    end
     @total_score
   end
 
   private
+  
+  def perfect_game?
+     @total_frames.max == @total_frames.min && @total_frames[0].rolls[0] == 10 
+  end
 
   def frame_score(frame, frame_index)
     @total_score += frame.rolls.sum
@@ -18,11 +28,12 @@ class Score
   end
 
   def extra_score(frame, frame_index)
-    if frame.type == 'spare'
+     if @total_frames[frame_index + 1].nil?
+         puts 'Game Finished!'
+    elsif frame.type == 'spare'
       @total_score += @total_frames[frame_index + 1].rolls[0]
     elsif frame.type == 'strike'
-      @total_score += @total_frames[frame_index + 1].rolls[0]
-      @total_score += @total_frames[frame_index + 1].rolls[1]
+      @total_score += @total_frames[frame_index + 1].rolls.sum
     end
   end
 
--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -14,8 +14,17 @@ describe Score do
       spare_frame = instance_double('spare_frame', :rolls => [9, 1], :type => 'spare')
       frames = [spare_frame]
       9.times { frames << basic_frame }
+      allow(:total_frames).to receive(:total_score).and_return(false)
 
       expect(subject.total_score(frames)).to eq 38
+    end
+    
+    it 'returns a total score of 135 if game is all spares' do
+        spare_frame = instance_double('spare_frame', :rolls => [9, 1], :type => 'spare')
+        frames = []
+        10.times { frames << spare_frame }
+        expect(subject.total_score(frames)).to eq(181)
+        
     end
     
     it 'returns total score for a game with a strike' do
@@ -25,5 +34,12 @@ describe Score do
         9.times {frames << basic_frame}
         expect(subject.total_score(frames)).to eq(40)
     end
+    
+    it 'returns final score of 300 for perfect game' do
+        strike_frame = instance_double('strike_frame', :rolls => [10], :type => 'strike')
+        frames = []
+        10.times {frames << strike_frame}
+        expect(subject.total_score(frames)).to eq(300)
+    end    
   end
 end
