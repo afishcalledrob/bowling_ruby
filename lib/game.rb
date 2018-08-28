be2ce9d11d3b539287fe6bf86ae727c3f5fa13f6
Robert <fishwickrob@live.com>
2018-08-28 12:38:14 +0000

    Initialises game with inital score of zero

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,9 @@
 
 class Game
-  attr_reader :total_frames
+  attr_reader :total_frames, :score
 
   def initialize
     @total_frames = []
+    @score = 0
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,5 +5,9 @@ describe Game do
     it 'initializes with an empty frame' do
       expect(subject.total_frames).to eq []
     end
+    
+    it 'initializes with an initial score of zero' do
+      expect(subject.score).to eq 0
+    end
   end  
 end
